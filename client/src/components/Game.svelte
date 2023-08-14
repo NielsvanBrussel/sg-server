6f77e66835dbb7362333853a6f0aa53be50a40f8
Niels van Brussel <niels_vanbrussel@hotmail.com>
2023-08-14 22:15:16 +0200

    overlay/inventory

--- client/src/components/Game.svelte
+++ client/src/components/Game.svelte
@@ -1,11 +1,10 @@
 <script>
-	import { avatar, activeScenario, menuActive } from '../stores.js';
+	import { avatar, activeScenario } from '../stores.js';
   	import CharacterCreation from './CharacterCreation.svelte';
   	import TextButton from './core/TextButton.svelte';
-	import hitpoints from '../assets/img/hitpoints.gif'
   	import CharacterDeath from './CharacterDeath.svelte';
   	import SideScroller from './SideScroller.svelte';
-	import { IconMenu2, IconMoneybag } from '@tabler/icons-svelte'; 
+  	import Overlay from './Overlay.svelte';
 
 	const leaveScenario = () => {
 		activeScenario.set({
@@ -13,17 +12,9 @@
 			component: null,
         })
 	}
-
 </script>
 
 
-<div class="button__menu__container">
-	<button class="menu-button" on:click={() => menuActive.set(true)}> 
-		
-	</button>
-	<button class="inventory-button" on:click={() => null}> 
-	</button>                
-</div>
 <div class="container">
 	{#if !$avatar.name}
 		<CharacterCreation />
@@ -31,33 +22,7 @@
 		{#if $avatar.stats.currentHitpoints < 1}
 			<CharacterDeath />
 		{:else}
-			<div class="stats-overlay-container">
-				<div class="flexbox hitpoints-container">
-					<img class="hitpoints-img" src={hitpoints} alt="hitpoints">
-					<p>{$avatar.stats.currentHitpoints} / {$avatar.stats.maxHitpoints}</p>
-				</div>
-				<div class="flexbox money-container">
-					<p class="dollar-sign">$</p>
-					<p>{$avatar.money}</p>
-				</div>
-				<div class="flexbox day-container">
-					{#if $avatar.day === 1}
-						<p>monday</p>
-					{:else if $avatar.day === 2}
-						<p>tuesday</p>
-					{:else if $avatar.day === 3}
-						<p>wednesday</p>
-					{:else if $avatar.day === 4}
-						<p>thursday</p>
-					{:else if $avatar.day === 5}
-						<p>friday</p>
-					{:else if $avatar.day === 6}
-						<p>saturday</p>
-					{:else if $avatar.day === 7}
-						<p>sunday</p>
-					{/if}
-				</div>
-			</div>
+			<Overlay />
 			{#if $activeScenario.component}
 				<div class="scenario-container">
 					<svelte:component this={$activeScenario.component} />
@@ -84,80 +49,6 @@
 		width: 90vw;
 		position: relative;
 		background-color: #03071E;
-	}
-	.button__menu__container {
-		position: absolute;
-		left: 0;
-		top: 9rem;
-		margin: 0rem 2rem;
-		z-index: 10;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		gap: 1rem;
-  	}
-	.menu-button, .inventory-button {
-		background-repeat: no-repeat;
-		background-position: center;
-		background-size: 2rem auto;
-		min-height: 3rem;
-		min-width: 4rem;
-		border: 3px solid transparent;
-		background-color: rgba(0, 0, 0, 0.5);
-	}
-	.menu-button:hover, .inventory-button:hover {
-		border-color: #F48C06;
-	}
-	.menu-button {
-		background-image: url('../assets/img/menu.png');
-	}
-	.inventory-button {
-		background-image: url('../assets/img/inventory.png');
-	}
-	.stats-overlay-container {
-		position: absolute;
-		height: 8rem;
-		padding: 1rem 0 0 0;
-		top: 0rem;
-		left: 0;
-		z-index: 5;
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-start;
-		align-items: flex-start;
-		width: 100%;
-		font-family: "PS2P";
-		font-size: 1.25rem;
-	}
-	.flexbox {
-		display: flex;
-		flex-direction: row;
-		gap: 1rem;
-		margin: 0rem 2rem;
-		align-items: center;
-	}
-	.money-container {
-		color: green;
-	}
-	.day-container {
-		color: #F48C06;
-		margin-left: 2.5rem;
-	}
-	.dollar-sign {
-		font-size: 2rem;
-		margin: 0 0.5rem;
-		padding: 0;
-	}
-	.hitpoints-container {
-		color: #CD173F;
-	}
-	.hitpoints-img {
-		height: 6vh;
-		width: auto;
-	}
-	p {
-		margin: 0;
-		padding: 0;
 	}
 	.scenario-container {
 		position: relative;
---
+++ client/src/components/Overlay.svelte
@@ -0,0 +1,135 @@
+<script>
+	import { avatar, menuActive } from '../stores.js';
+	import hitpoints from '../assets/img/hitpoints.gif'
+	import inventoryIcon from '../assets/img/inventory.png'
+	import inventoryOpenIcon from '../assets/img/inventory-open.png'
+	import menuIcon from '../assets/img/menu.png'
+	import statsIcon from '../assets/img/stats.png'
+
+
+	let inventoryActive = false
+	let statsActive = false
+
+</script>
+
+<div class="flexbox-overlay">
+    <div class="button__menu__container">
+        <button class="menu-button" on:click={() => menuActive.set(true)}>
+            <img src={menuIcon} alt='menu' class="menu-img">
+        </button>
+        <button class="stats-button" on:click={() => statsActive = !statsActive}>
+            <img src={statsIcon} alt='stats' class="menu-img">
+        </button>
+        <button class="inventory-button" on:click={() => inventoryActive = !inventoryActive}>
+            <img src={inventoryActive ? inventoryOpenIcon : inventoryIcon} alt='inventory' class="menu-img">
+        </button>                
+    </div>	
+    <div class="stats-overlay-container">
+        <div class="flexbox hitpoints-container">
+            <img class="hitpoints-img" src={hitpoints} alt="hitpoints">
+            <p>{$avatar.stats.currentHitpoints} / {$avatar.stats.maxHitpoints}</p>
+        </div>
+        <div class="flexbox money-container">
+            <p class="dollar-sign">$</p>
+            <p>{$avatar.money}</p>
+        </div>
+        <div class="flexbox day-container">
+            {#if $avatar.day === 1}
+                <p>monday</p>
+            {:else if $avatar.day === 2}
+                <p>tuesday</p>
+            {:else if $avatar.day === 3}
+                <p>wednesday</p>
+            {:else if $avatar.day === 4}
+                <p>thursday</p>
+            {:else if $avatar.day === 5}
+                <p>friday</p>
+            {:else if $avatar.day === 6}
+                <p>saturday</p>
+            {:else if $avatar.day === 7}
+                <p>sunday</p>
+            {/if}
+        </div>
+    </div>		
+</div>
+
+
+
+
+<style>
+
+
+	.flexbox-overlay {
+		display: flex;
+		flex-direction: row;
+		position: absolute;
+		top: 1rem;
+		left: 0;
+		z-index: 10;
+	}
+	.button__menu__container {
+		position: relative;
+		margin: 0 0 0 2rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 0.25rem;
+  	}
+	.menu-button, .inventory-button, .stats-button {
+		border: 3px solid transparent;
+		background-color: rgba(0, 0, 0, 0.3);
+		padding: 0.25rem 0.5rem;
+		display: flex;
+		justify-content: center;
+		flex-direction: row;
+	}
+	.menu-img {
+		height: 1.75rem;
+		width: 1.75rem;
+	}
+	.menu-button:hover, .inventory-button:hover, .stats-button:hover {
+		border-color: #F48C06;
+	}
+	.stats-overlay-container {
+		height: 9rem;
+		position: relative;
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
+		font-family: "PS2P";
+		font-size: 1.25rem;
+	}
+	.flexbox {
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+		margin: 0rem 1rem;
+		align-items: center;
+	}
+	.money-container {
+		color: green;
+	}
+	.day-container {
+		color: #F48C06;
+		margin-left: 1.5rem;
+	}
+	.dollar-sign {
+		font-size: 2rem;
+		margin: 0 0.5rem;
+		padding: 0;
+	}
+	.hitpoints-container {
+		color: #CD173F;
+	}
+	.hitpoints-img {
+		height: 5vh;
+		width: auto;
+	}
+	p {
+		margin: 0;
+		padding: 0;
+	}
+
+</style>
